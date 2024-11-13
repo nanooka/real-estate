3b368615b4ec312a094bd27406bc5902dc7342f7
nanooka <102428722+nanooka@users.noreply.github.com>
2024-11-13 15:40:14 +0400

    submodule and some routes

--- api/app.js
+++ api/app.js
@@ -1,17 +1,21 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import postRouter from "./routes/post.route.js";
-import authRouter from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRouter);
-app.use("/api/auth", authRouter);
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on 8800");
 });
-// 55:00
+// 01:39
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,10 +52,13 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       {
         id: user.id,
+        isAdmin: false,
       },
       process.env.JWT_SECRET_KEY,
       { expiresIn: age }
     );
+
+    const { password: userPassword, ...userInfo } = user;
 
     res
       .cookie("token", token, {
@@ -64,7 +67,7 @@ export const login = async (req, res) => {
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Successfully" });
+      .json(userInfo);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to log in" });
