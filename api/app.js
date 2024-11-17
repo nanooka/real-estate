1056af14a6fb05e86d2393ad5ae00ee6becae89c
nanooka <102428722+nanooka@users.noreply.github.com>
2024-11-17 15:15:01 +0400

    chat and message routes

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
@@ -16,8 +18,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on 8800");
 });
-// 03:22
+// 04:08
