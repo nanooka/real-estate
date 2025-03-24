863ecd710049b4f0800415917723cda5269d16f8
nanooka <102428722+nanooka@users.noreply.github.com>
2025-03-24 13:53:35 -0400

    testing socket

--- socket/app.js
+++ socket/app.js
@@ -6,12 +6,6 @@ import { Server } from "socket.io";
 //   },
 // });
 
-// const io = new Server({
-//   cors: {
-//     origin: "https://real-estate-nanooka.netlify.app",
-//   },
-// });
-
 const io = new Server({
   cors: {
     origin: "https://real-estate-nanooka.netlify.app",
