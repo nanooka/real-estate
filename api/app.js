e3c20aa984f6b813d90ff21a53f468cf3bb2b2a5
nanooka <102428722+nanooka@users.noreply.github.com>
2025-04-06 15:56:53 -0400

    token error on deployment

--- api/app.js
+++ api/app.js
@@ -27,7 +27,20 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+// app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
