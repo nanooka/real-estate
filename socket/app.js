e4dafea6878bdbf0bfc6c406757b218383d765a8
nanooka <102428722+nanooka@users.noreply.github.com>
2025-04-06 14:19:14 -0400

    updated user profileposts and socket

--- socket/app.js
+++ socket/app.js
@@ -1,8 +1,8 @@
-import { Server } from "socket.io";
-// import "dotenv/config";
+// import { Server } from "socket.io";
+// // import "dotenv/config";
 
-// const URL = process.env.CLIENT_URL_LOCAL;
-// const URL = process.env.CLIENT_URL_DEPLOYMENT
+// // const URL = process.env.CLIENT_URL_LOCAL;
+// // const URL = process.env.CLIENT_URL_DEPLOYMENT
 
 // const io = new Server({
 //   cors: {
@@ -10,23 +10,69 @@ import { Server } from "socket.io";
 //   },
 // });
 
-const io = new Server({
+// // const io = new Server({
+// //   cors: {
+// //     origin: "https://real-estate-nanooka.netlify.app",
+// //   },
+// // });
+
+// // const io = new Server({
+// //   cors: {
+// //     origin: URL,
+// //   },
+// // });
+
+// let onlineUser = [];
+
+// const addUser = (userId, socketId) => {
+//   const userExists = onlineUser.find((user) => user.userId === userId);
+
+//   if (!userExists) {
+//     onlineUser.push({ userId, socketId });
+//   }
+// };
+
+// const removeUser = (socketId) => {
+//   onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+// };
+
+// const getUser = (userId) => {
+//   return onlineUser.find((user) => user.userId === userId);
+// };
+
+// io.on("connection", (socket) => {
+//   socket.on("newUser", (userId) => {
+//     addUser(userId, socket.id);
+//   });
+
+//   socket.on("sendMessage", ({ receiverId, data }) => {
+//     const receiver = getUser(receiverId);
+//     io.to(receiver.socketId).emit("getMessage", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     removeUser(socket.id);
+//   });
+// });
+
+// io.listen("4000", () => {
+//   console.log(`Socket.IO server running on port 4000`);
+// });
+
+import { Server } from "socket.io";
+
+// Initialize Socket.IO server
+const io = new Server(4000, {
   cors: {
-    origin: "https://real-estate-nanooka.netlify.app",
+    origin: "http://localhost:5173", // Frontend URL
   },
 });
 
-// const io = new Server({
-//   cors: {
-//     origin: URL,
-//   },
-// });
-
 let onlineUser = [];
 
+// Helper functions
 const addUser = (userId, socketId) => {
   const userExists = onlineUser.find((user) => user.userId === userId);
-
   if (!userExists) {
     onlineUser.push({ userId, socketId });
   }
@@ -40,14 +86,20 @@ const getUser = (userId) => {
   return onlineUser.find((user) => user.userId === userId);
 };
 
+// WebSocket events
 io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
   socket.on("newUser", (userId) => {
     addUser(userId, socket.id);
   });
 
   socket.on("sendMessage", ({ receiverId, data }) => {
     const receiver = getUser(receiverId);
-    io.to(receiver.socketId).emit("getMessage", data);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+      // updateLastMessage(receiverId, data.text);
+    }
   });
 
   socket.on("disconnect", () => {
@@ -55,6 +107,4 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen("4000", () => {
-  console.log(`Socket.IO server running on port 4000`);
-});
+console.log("Socket.IO server running on port 4000");
